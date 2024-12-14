2d6342b85d6f8d23691594bcb5eb7dab6d366dda
𝗪𝗮𝗹𝗸-𝗗𝗿𝗼𝘃𝗲 <farhanali28690@gmail.com>
2024-12-14 10:41:08 +0500

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -15,7 +15,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *PANHWAR MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©PANHWAR MD BY Ansar Panhwar*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *DROVE MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©DROVE MD BY M.Farhan Ali*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
