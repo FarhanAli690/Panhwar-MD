00a0f2b0daa9d4600952cf716d429c274c69f405
𝗪𝗮𝗹𝗸-𝗗𝗿𝗼𝘃𝗲 <farhanali28690@gmail.com>
2024-12-14 17:57:00 +0500

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -13,16 +13,16 @@ try{
 let repo =`
 *╭──────────────●●►*
 > *BOT OWNER:*
-*|* *ANSAR PANHWAR*
+*|* *M.Farhan Ali*
 
 > *PANHWAR MD REPO:*
-*|* *https://github.com/Panhwar110/Panhwar-MD*
+*|* *https://github.com/FarhanAli690/DROVE-MD*
 
 > *SUPPORT CHANNEL:*
-*|* *https://whatsapp.com/channel/0029ValASu1IN9ifummBKW1U*
+*|* *https://whatsapp.com/channel/0029Vank3vaLikg5ldoNdd0U*
 *╰──────────────●●►*
 
-> *update command Done*
+> *POWERED BY: 'WALK-DROVE'*
 `
 await conn.sendMessage(from, { text: repo ,
   contextInfo: {
@@ -36,11 +36,11 @@ await conn.sendMessage(from, { text: repo ,
       serverMessageId: 999
     },
 externalAdReply: { 
-title: 'PANHWAR MD',
+title: 'DROVE MD',
 body: `${pushname}`,
 mediaType: 1,
-sourceUrl: "https://github.com/Panhwar110/Panhwar-MD" ,
-thumbnailUrl: "https://i.ibb.co/7NKFsxS/IMG-20241119-WA0087.jpg" ,
+sourceUrl: "https://github.com/FarhanAli690/DROVE-MD" ,
+thumbnailUrl: "https://i.ibb.co/Zdjh4Qs/IMG-20241113-WA0019.jpg" ,
 renderLargerThumbnail: true,
 showAdAttribution: true
 }
